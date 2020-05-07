d9496d04e419221150c5dc9e6a76a3121700db16
jonasa64 <hvidovrefifa@gmail.com>
2020-05-07 14:19:03 +0200

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('../util/database');
+
+sequelize.sync();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('I am working');
+})
+
+app.listen(port, () => {
+    console.log(`serve is runing on port ${port}`);
+})
