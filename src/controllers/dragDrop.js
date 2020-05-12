f58bfaa6f0dd9ed197e3c3bc1f4746bb6b1a790e
jonasa64 <hvidovrefifa@gmail.com>
2020-05-12 15:08:02 +0200

    update dragDrop

--- src/controllers/dragDrop.js
+++ src/controllers/dragDrop.js
@@ -24,3 +24,27 @@ exports.create = (req, res) => {
     })
 }
 
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    DragDrop.update({
+        question: req.body.question,
+        type: req.body.type
+    }, {where:{id: id}})
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Drag and Drop was updated successfully"
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Drag and Drop with id=${id}. Maybe Drag and Drop was not found or req.body is empty!`
+                })
+            }
+        }).catch(err => {
+        res.send(500).send({
+            message: "Error updating Drag and Drop with id=" + id
+        });
+    })
+}
