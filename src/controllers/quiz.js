8bb4475eaf47edc00f87138be17467693308c239
jonasa64 <hvidovrefifa@gmail.com>
2020-05-13 16:06:22 +0200

    delete and update

--- src/controllers/quiz.js
+++ src/controllers/quiz.js
@@ -27,3 +27,50 @@ exports.findAll = (req, res) => {
             });
         })
 }
+
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Quiz.destroy({
+        where: { id: id }
+    }).then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Quiz was deleted successfully!"
+            })
+        } else {
+            res.send({
+                message: `Cannot delete Quiz with id=${id}. Maybe Quiz was not found!`
+            })
+        }
+    })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Quiz with id=" + id
+            })
+        })
+}
+
+exports.update = (req, res) => {
+   const question = req.body.question;
+   const id = req.params.id;
+    Quiz.update({
+        Question: question
+    },{where: {id:id}})
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Quiz was updated successfully!"
+                })
+            } else {
+                res.send({
+                    message: `Cannot update Quiz with id=${id}. Maybe Quiz was not found!`
+                })
+            }
+        }).catch(err => {
+        res.status(500).send({
+            message: "Could not update Quiz with id=" + id
+        })
+    })
+}
