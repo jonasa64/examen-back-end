e0fd65bca02b03298d2b3cb23200e3ccdf39a8ef
jonasa64 <hvidovrefifa@gmail.com>
2020-05-14 11:21:56 +0200

    removed some stuff

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -3,49 +3,36 @@ const orderDetails = require('../models/OrderDeatils');
 const Customer = require('../models/Customer');
 const shirt = require('../models/Tshirt');
 exports.create = async (req, res) => {
-try {
+    try {
 
-    const newOrder =   await  order.create({
-        total_price : req.body.totalPrice,
-        customerCId : req.body.customerId
-    });
-    let key = 'orderId';
-    req.body.od.map((el,index) => {
-        el[key] = newOrder.id;
-    })
-    console.log(req.body.od);
+        const newOrder = await order.create({
+            total_price: req.body.totalPrice,
+            customerCId: req.body.customerId
+        });
+        let key = 'orderId';
+        req.body.od.map((el, index) => {
+            el[key] = newOrder.id;
+        })
+        console.log(req.body.od);
 
-   const od = await
-        orderDetails.bulkCreate(
-            req.body.od
-        );
+        const od = await
+            orderDetails.bulkCreate(
+                req.body.od
+            );
 
 
 
-    if(od){
-        return res.send('order is created');
+        if (od) {
+            return res.send('order is created');
+        }
+
+
+    } catch (e) {
+        res.status(500).send({
+            message: e.message || "Some error occuredd while creating the order"
+        });
     }
 
-
-}catch (e) {
-    res.status(500).send({
-        message: e.message || "Some error occuredd while creating the order"
-    });
-}
-
-
-    orderDetails.bulkCreate({
-        quantity: req.body.od.quantity,
-        orderId : newOrder.id,
-        shirtId : req.body.od.shirtId
-    }).then(od => {
-        console.log(od);
-        res.send('order created');
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occuredd while creating the order"
-        });
-    });
 
 }
 
@@ -53,7 +40,7 @@ try {
 exports.delete = (req, res) => {
     const id = req.params.id;
 
-    order.destroy({where: {id: id}}).then(num => {
+    order.destroy({ where: { id: id } }).then(num => {
         if (num == 1) {
             res.send({
                 message: "order was deleted successfully!"
@@ -78,7 +65,7 @@ exports.update = (req, res) => {
         order_status: req.body.orderStatus,
         shipping_status: req.body.shippingStatus
     }, {
-        where : {id: id}
+        where: { id: id }
     }).then(num => {
         if (num == 1) {
             res.send({
@@ -97,7 +84,7 @@ exports.update = (req, res) => {
 }
 
 exports.findAll = (req, res) => {
-    order.findAll({include: [orderDetails, Customer]}).then(orders => {
+    order.findAll({ include: [orderDetails, Customer] }).then(orders => {
         res.send(orders);
     }).catch(err => {
         res.status(500).send({
@@ -109,7 +96,7 @@ exports.findAll = (req, res) => {
 exports.findOne = (req, res) => {
 
     const id = req.params.id;
-    order.findByPk(id,{ include: [orderDetails, Customer]}).then(order => {
+    order.findByPk(id, { include: [orderDetails, Customer] }).then(order => {
         res.send(order);
     }).catch(err => {
         res.status(500).send({
@@ -121,7 +108,7 @@ exports.findOne = (req, res) => {
 
 exports.findByUserId = (req, res) => {
     const customerId = req.params.customerId;
-    order.findAll( {where : {customerCId: customerId}, include: [orderDetails, Customer]}).then(orders => {
+    order.findAll({ where: { customerCId: customerId }, include: [orderDetails, Customer] }).then(orders => {
         res.send(orders);
     }).catch(err => {
         res.status(500).send({
