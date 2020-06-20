dc82161c7a6f9b74704ae851b19877bc378c60e9
jonasa64 <hvidovrefifa@gmail.com>
2020-06-20 11:17:57 +0200

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-const databaseName =  process.env.DBNAME;
-const databaseUser =  process.env.DBUSER;
-const databasePassword =  process.env.DBPASSWORD;
-const databaseHost =  process.env.DBHOST;
+const databaseName =  process.env.DBNAME || 'examen';
+const databaseUser =  process.env.DBUSER || 'root';
+const databasePassword =  process.env.DBPASSWORD || '';
+const databaseHost =  process.env.DBHOST || 'localhost';
 
 const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
     dialect: 'mysql',
